22c1b52662f2616da90d71b423e21aeaca8ab3ed
porcupiggy <kendrachev.7@gmail.com>
2026-03-31 21:27:42 -0300

    Create crops.js with crop data
    Added an array of crop objects with details such as id, name, season, price, latitude, longitude, and image URL.

---
+++ crops.js
@@ -0,0 +1,47 @@
+export const crops = [
+    {
+        id: 1,
+        name: "Parsnip",
+        season: "Spring",
+        price: 35,
+        lat: 45.0,
+        lng: -63.0,
+        image: "https://stardewvalleywiki.com/mediawiki/images/6/68/Parsnip.png"
+    },
+    {
+        id: 2,
+        name: "Blueberry",
+        season: "Summer",
+        price: 50,
+        lat: 40.0,
+        lng: -120.0,
+        image: "https://stardewvalleywiki.com/mediawiki/images/5/5c/Blueberry.png"
+    },
+    {
+        id: 3,
+        name: "Pumpkin",
+        season: "Fall",
+        price: 320,
+        lat: 51.0,
+        lng: 10.0,
+        image: "https://stardewvalleywiki.com/mediawiki/images/6/64/Pumpkin.png"
+    },
+    {
+        id: 4,
+        name: "Strawberry",
+        season: "Spring",
+        price: 120,
+        lat: -33.0,
+        lng: 151.0,
+        image: "https://stardewvalleywiki.com/mediawiki/images/6/6d/Strawberry.png"
+    },
+    {
+        id: 5,
+        name: "Ancient Fruit",
+        season: "All Seasons",
+        price: 550,
+        lat: 35.0,
+        lng: 139.0,
+        image: "https://stardewvalleywiki.com/mediawiki/images/0/04/Ancient_Fruit.png"
+    }
+];
